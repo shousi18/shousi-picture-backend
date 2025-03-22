7ab7aa40941f2ee437c9d9ade507531be3993975
贺羿鼎 <13204855+shou_si@user.noreply.gitee.com>
2025-03-23 00:40:30 +0800

    feat:补充上一次提交内容

---
+++ src/main/java/com/shousi/web/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.shousi.web.mapper;
+
+import com.shousi.web.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 86172
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-03-22 23:29:35
+* @Entity com.shousi.web.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
